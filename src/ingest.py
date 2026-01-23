7c033fcee5f5ea57c1fdfc8ba73718af5697df30
Vander Nunes <tec.vander.nunes@gmail.com>
2026-01-22 21:51:51 -0300

    Ingestão: Ler um arquivo PDF e salvar suas informações em um banco de dados PostgreSQL com extensão pgVector.

--- src/ingest.py
+++ src/ingest.py
@@ -1,13 +1,59 @@
 import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+
+from langchain_core.documents import Document
+from langchain_postgres import PGVector
+
 
 load_dotenv()
 
+for k in ("OPENAI_API_KEY", "DATABASE_URL","PG_VECTOR_COLLECTION_NAME", "PDF_PATH"):
+    if not os.getenv(k):
+        raise RuntimeError(f"Environment variable {k} is not set")
+
+
 PDF_PATH = os.getenv("PDF_PATH")
 
-def ingest_pdf():
-    pass
+loader = PyPDFLoader(PDF_PATH)
+documents = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000,
+    chunk_overlap=150,
+    add_start_index=False
+)
+
+chunks = splitter.split_documents(documents)
+
+if not chunks:
+    raise SystemError(0)
 
 
-if __name__ == "__main__":
-    ingest_pdf()
+enriched = [
+    Document(
+        page_content=d.page_content,
+        metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+    )
+    for d in chunks
+]    
+
+ids = [f"doc-{i}" for i in range(len(enriched))]
+
+embeddings = OpenAIEmbeddings(model=os.getenv("OPENAI_MODEL","text-embedding-3-small"))
+
+# embeddings = GoogleGenerativeAIEmbeddings(model=os.getenv("GOOGLE_EMBEDDING_MODEL","models/embedding-001"))
+
+store = PGVector(
+    embeddings=embeddings,
+    collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+    connection=os.getenv("DATABASE_URL"),
+    use_jsonb=True,
+)
+
+store.add_documents(documents=enriched, ids=ids)
+
+print("*** Ingestão finalizada com sucesso! Vetores armazenados no Postgres/pgvector. ***")
